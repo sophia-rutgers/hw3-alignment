b8965e671e909b73f8e1b794ad21f15093361c0c
sophia-rutgers <sophia.tan@rutgers.edu>
2017-02-25 01:53:57 -0800

    restructured repo by putting python scripts into scripts sub-directory, modified requirements.txt

---
+++ README.md
@@ -0,0 +1,26 @@
+# HW2 Skeleton
+
+BUILD STATUS:
+https://travis-ci.org/sophia-rutgers/hw3-alignment
+
+## usage
+To use the package, first run
+```
+conda install --yes --file requirements.txt
+```
+to install all the dependencies in `requirements.txt`. 
+
+Then, you can run the code by
+```
+python scripts/run_alignment.py 
+```
+
+## testing
+
+Testing is as simple as running
+```
+python -m pytest
+```
+from the root directory of this project.
+
+
--- tests/test_hw3.py
+++ tests/test_hw3.py
@@ -1,37 +1,84 @@
 import sys
 import numpy as np, matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve,auc
-from alignment_HW3 import HelperFunctions
+from scripts import HelperFunctions
 
 def test_seq_io():
-    seq1,seq2=read_seq('sequences/prot-0004.fa','sequences/prot-0008.fa')
+    seq1,seq2=HelperFunctions.read_seq('sequences/prot-0004.fa','sequences/prot-0008.fa')
     assert seq1 == 'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM'
     assert seq2 == 'ANKTRELCMKSLEHAKVDTSNEARQDGIDLYKHMFENYPPLRKYFKSREEYTAEDVQNDPFFAKQGQKILLACHVLCATYDDRETFNAYTRELLDRHARDHVHMPPEVWTDFWKLFEEYLGKKTTLDEPTKQAWHEIGREFAKEINK'
 
 def test_matrix_io():
-    BLOSUM50 = read_matrix('BLOSUM50')
+    BLOSUM50 = HelperFunctions.read_matrix('BLOSUM50')
     assert BLOSUM50[0] == [5, -2, -1, -2, -1, -1, -1, 0, -2, -1, -2, -1, -1, -3, -1, 1, 0, -3, -2, 0, -2, -1, -1, -5]
     
 def test_match_score():
-    BLOSUM50 = read_matrix('BLOSUM50')
-    score = match_score('P','K',BLOSUM50) 
+    BLOSUM50 = HelperFunctions.read_matrix('BLOSUM50')
+    score = HelperFunctions.match_score('P','K',BLOSUM50) 
     assert score == -1
 
 def test_sw_alignment_part1():
-    seq1,seq2=read_seq('sequences/prot-0004.fa','sequences/prot-0008.fa')
-    BLOSUM50 = read_matrix('BLOSUM50')
-    score,align1,align2 = alignment(seq1,seq2,-5,-1,BLOSUM50)
+    seq1,seq2=HelperFunctions.read_seq('sequences/prot-0004.fa','sequences/prot-0008.fa')
+    BLOSUM50 = HelperFunctions.read_matrix('BLOSUM50')
+    score,align1,align2 = HelperFunctions.alignment(seq1,seq2,-5,-1,BLOSUM50)
     print(score)
     assert score == 123.0
 
 def test_sw_alignment_part2():
     '''Try with different gap penalties'''
-    seq1,seq2=read_seq('sequences/prot-0004.fa','sequences/prot-0008.fa')
-    BLOSUM50 = read_matrix('BLOSUM50')
-    score,align1,align2 = alignment(seq1,seq2,-10,-5,BLOSUM50)
+    seq1,seq2=HelperFunctions.read_seq('sequences/prot-0004.fa','sequences/prot-0008.fa')
+    BLOSUM50 = HelperFunctions.read_matrix('BLOSUM50')
+    score,align1,align2 = HelperFunctions.alignment(seq1,seq2,-10,-5,BLOSUM50)
     assert score == 46.0
 
+def test_sim_annealing():
+    BLOSUM50 = HelperFunctions.read_matrix('BLOSUM50')
+    pospairs = []
+    negpairs = []
+    with open('Pospairs.txt', 'r') as f1:
+        for line in f1:
+            line = line.strip()
+            line = line.split(' ')
+            pospairs.append(line)
+    with open('Negpairs.txt', 'r') as f2:
+        for line in f2:
+            line = line.strip()
+            line = line.split(' ')
+            negpairs.append(line)
+    score_pos,static_align1_pos,static_align2_pos = HelperFunctions.align_set(pospairs,-7,-3,BLOSUM50)
+    score_neg,static_align1_neg,static_align2_neg = HelperFunctions.align_set(negpairs,-7,-3,BLOSUM50)
+    static = []
+    static.append(static_align1_pos)
+    static.append(static_align2_pos)
+    static.append(static_align1_neg)
+    static.append(static_align2_neg)
+    obj_values = HelperFunctions.sim_annealing(static,BLOSUM50,10)[0]
+    # check that there are 4013 iterations
+    assert len(obj_values) == 4013 
+    # check that there are 2 values per iteration (old objective value, new objective value)
+    assert len(obj_values[0]) == 2
+
 def test_symmetric_optimized_matrix():
-    optimized_matrix = sim_annealing(static,BLOSUM50,5)[1]
+    BLOSUM50 = HelperFunctions.read_matrix('BLOSUM50')
+    pospairs = []
+    negpairs = []
+    with open('Pospairs.txt', 'r') as f1:
+        for line in f1:
+            line = line.strip()
+            line = line.split(' ')
+            pospairs.append(line)
+    with open('Negpairs.txt', 'r') as f2:
+        for line in f2:
+            line = line.strip()
+            line = line.split(' ')
+            negpairs.append(line)
+    score_pos,static_align1_pos,static_align2_pos = HelperFunctions.align_set(pospairs,-7,-3,BLOSUM50)
+    score_neg,static_align1_neg,static_align2_neg = HelperFunctions.align_set(negpairs,-7,-3,BLOSUM50)
+    static = []
+    static.append(static_align1_pos)
+    static.append(static_align2_pos)
+    static.append(static_align1_neg)
+    static.append(static_align2_neg)
+    optimized_matrix = HelperFunctions.sim_annealing(static,BLOSUM50,5)[1]
     assert optimized_matrix[6][3] == optimized_matrix[3][6]
     assert optimized_matrix[12][4] == optimized_matrix[4][12]
